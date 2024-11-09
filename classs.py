97dbfe60b8e308dbd72d007f77b7a0c78a142c18
Imad Afif <imadafif.00@gmail.com>
2024-11-09 06:51:35 -0600

    adding multithreading

--- classs.py
+++ classs.py
@@ -1,18 +1,83 @@
 # classs.py
 import struct
+import time
+from colorama import Fore,init
+import threading
+import socket
+import queue
+class PacketSniffer:
+    def __init__(self, interface):
+        self.interface = interface
+        self.pckt_queue = queue.Queue()
+        self.num_worker_threads = 2
+        self.processing_threads = []
+        self.running = True  # Flag to control thread execution
 
+    def packet_capture(self):
+        try:
+            ETH_P_ALL = 0x03
+            s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(ETH_P_ALL))
+            s.bind((self.interface, 0))
+            
+            while self.running:
+                raw_data, addr = s.recvfrom(65535)
+                self.pckt_queue.put(raw_data)
+        except Exception as e:
+            print(f"Error in packet_capture: {e}")
+
+    def packet_processing(self):
+        while self.running:
+            try:
+                raw_data = self.pckt_queue.get(timeout=1)  # Timeout allows thread to check the running flag
+                eth = EthernetFrame(raw_data)  # Parse the Ethernet frame
+
+                # Print Ethernet frame details
+                print(Fore.GREEN + f"[*] Ethernet - {hex(eth.ETHER_TYPE)}; Source: {eth.SOURCE}; Dest: {eth.DESTINATION}; Data: {eth.Data[:20]}...")
+                self.pckt_queue.task_done()
+
+                if eth.ETHER_TYPE == IPV4.ID:
+                    ipv4 = IPV4(eth.Data)
+                    print(Fore.YELLOW +"->" + ipv4.parse_())  # Use the parse_ method of the IPV4 instance
+                    if ipv4.PROTOCOL == TCP.ID:
+                        tcp = TCP(ipv4.PAYLOAD)
+                        print(Fore.CYAN +"   └─ " + tcp.parse_tcp())
+                    elif ipv4.PROTOCOL == UDP.ID:
+                        udp = UDP(ipv4.PAYLOAD)
+                        print(Fore.CYAN +"   └─ " + udp.parse_udp())
+            except queue.Empty:
+                pass  # Allows the thread to continue checking if there are new packets
+            except Exception as e:
+                print(f"Error in packet_processing: {e}")
+
+    def start_sniff(self):
+        try:
+            capture_thread = threading.Thread(target=self.packet_capture, daemon=True)
+            capture_thread.start()
+
+            for _ in range(self.num_worker_threads):
+                process_thread = threading.Thread(target=self.packet_processing, daemon=True)
+                process_thread.start()
+                self.processing_threads.append(process_thread)
+
+            while self.running:
+                time.sleep(0.1)  
+            
+        except KeyboardInterrupt:
+            print("\n[!] Stopping sniffer...")
+            self.running = False
+            for thread in self.processing_threads:
+                thread.join() 
+            print("[:)] THALLAAA")
 class EthernetFrame:
     IPV4_ETHERTYPE = 0x0800  # EtherType for IPv4
 
     def __init__(self, data):
-        # Parse Ethernet frame
         dest, src, proto, payload = self.ethernet_head(data)
         self.DESTINATION = dest
         self.SOURCE = src
         self.ETHER_TYPE = proto
         self.Data = payload
         
-        # Parse IPv4 header if EtherType indicates IPv4
         self.ipv4_packet = None
         if self.ETHER_TYPE == self.IPV4_ETHERTYPE:
             self.ipv4_packet = IPV4(payload)
@@ -27,7 +92,7 @@ class EthernetFrame:
         src_mac = self.get_mac_addr(src)
         payload = raw_data[14:]
         return dest_mac, src_mac, proto, payload
-    def  parse_(self): # method used to define the string representation of an object
+    def  parse_(self): 
         ether = hex(self.ETHER_TYPE)
         source = self.mac_to_str(self.SOURCE)
         dest = self.mac_to_str(self.DESTINATION)
@@ -39,7 +104,6 @@ class EthernetFrame:
 class IPV4:
     ID = 0x0800
     def __init__(self, data):
-        # Parse IPv4 header
         VER_IHL, DSCP_ECN, LEN, ID, FLAGS_OFFSET, TTL, PROTO, CHECKSUM, SOURCE, DEST, payload = self.ipv4_head(data)
 
         # Extract version and IHL
@@ -77,7 +141,7 @@ class IPV4:
 class TCP:
     ID = 0x06
     def __init__(self, data):
-        SOURCE_PORT, DEST_PORT, SEQUENCE_NUM, ACK_NUM, OFFSET_FLAGS, WINDOW_SIZE, CHECKSUM, URGENT_POINTER, LEFTOVER= self.tcp_head(data)
+        SOURCE_PORT, DEST_PORT, SEQUENCE_NUM, ACK_NUM, OFFSET_FLAGS, WINDOW_SIZE, CHCKSUM, URGENT_POINTER, LEFTOVER= self.tcp_head(data)
         
         self.SRC_PORT = SOURCE_PORT
         self.DST_PORT = DEST_PORT
@@ -100,12 +164,11 @@ class TCP:
         
         self.WIN_SIZE = WINDOW_SIZE
         
-        self.CHKSUM  = CHECKSUM
+        self.CHKSUM  = CHCKSUM
         
         self.URG_PTR= URGENT_POINTER
         options_len = 0
         if self.OFFSET > 5:
-            # This line calculates the length of the options field in bytes.
             options_len = (self.OFFSET - 5) * 4 
         self.PARAMS = LEFTOVER[:options_len]
         self.PAYLOAD = LEFTOVER[options_len:]
@@ -117,7 +180,22 @@ class TCP:
         for key in self.FLAGS:
             if self.FLAGS[key]:
                 flags.append(key)
-        return f"[ TCP - Source Port: {self.SRC_PORT}; Destination Port: {self.DST_PORT}; Sequence: {self.SEQ_NUM}; ACK: {self.ACK_NUM}; Flags: {flags} ]"
+        return f" TCP - Source Port: {self.SRC_PORT}; Destination Port: {self.DST_PORT}; Sequence: {self.SEQ_NUM}; ACK: {self.ACK_NUM}; Flags: {flags} "
 class UDP:
     ID = 0x11
-    
+    def __init__(self, data):
+        SOURCE_PORT, DEST_PORT, LEN, CHKSUM, PAYLOAD = self.udp_head(data)
+        self.SRC_PORT = SOURCE_PORT
+        self.DST_PORT = DEST_PORT
+        self.LENGTH = LEN
+        self.CHKSUM = CHKSUM
+        self.PAYLOAD = PAYLOAD
+
+    def udp_head(self, data):
+        """Unpack the UDP header from the data."""
+        SRC_PORT, DST_PORT, LENGTH, CHKSUM = struct.unpack('! H H H H', data[:8])
+        return SRC_PORT, DST_PORT, LENGTH, CHKSUM, data[8:]
+
+    def parse_udp(self):
+        """Return a string representation of the UDP packet."""
+        return f" UDP - Source Port: {self.SRC_PORT}; Destination Port: {self.DST_PORT}; Length: {self.LENGTH}"
--- sniff.py
+++ sniff.py
@@ -1,10 +1,7 @@
-import socket
+
 import argparse
-import struct
-import sys
-from classs import EthernetFrame,IPV4,TCP
-from colorama import Fore,init
-init()
+from classs import PacketSniffer
+
 def get_args():
     parser = argparse.ArgumentParser(description="A simple network sniffer")
     parser.add_argument('interface', help="Network interface to sniff on")
@@ -12,24 +9,8 @@ def get_args():
     
 
 def main(interface):
-   ETH_P_ALL = 0x03
-   s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(ETH_P_ALL))
-   s.bind((interface,0))
-   while True:
-        raw_data, addr = s.recvfrom(65535)  # Capture packet data
-        eth = EthernetFrame(raw_data)  # Parse the Ethernet frame
-
-        # Print Ethernet frame details
-        print(Fore.GREEN +f"[*] Ethernet - {hex(eth.ETHER_TYPE)}; Source: {eth.SOURCE}; Dest: {eth.DESTINATION}; Data: {eth.Data[:20]}... ")
-
-        if eth.ETHER_TYPE == IPV4.ID:
-            ipv4 = IPV4(eth.Data)
-            print("->" + ipv4.parse_())  # Use the parse_ method of the IPV4 instance
-            if ipv4.PROTOCOL == TCP.ID:
-                    tcp = TCP(ipv4.PAYLOAD)
-                    print("   └─ " + tcp.parse_tcp())
-                    
-        print("-" * 50)  
+    sniffer = PacketSniffer(interface)
+    sniffer.start_sniff()
 if __name__ == "__main__":
    args=get_args()
    main(args.interface)
